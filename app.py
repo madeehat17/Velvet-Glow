82bc18077cede5f64d881ddc5838621b8a2a6982
madeehat17 <madeeha.t.1.7@gmail.com>
2026-02-05 11:37:51 +0500

    app.py

--- app.py
+++ app.py
@@ -109,8 +109,5 @@ def skincare():
     products = load_products("skincare")
     return render_template("skincare.html", products=products)
 
-import os
-
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Use Render's PORT or default to 5000 locally
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(debug=True)
