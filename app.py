e597cef8ef62b1e06a530fbd2fc1314bab55c699
madeehat17 <madeeha.t.1.7@gmail.com>
2026-02-05 12:10:55 +0500

    app.py

--- app.py
+++ app.py
@@ -109,9 +109,7 @@ def skincare():
     products = load_products("skincare")
     return render_template("skincare.html", products=products)
 
-import os
-
 if __name__ == "__main__":
+    import os
     port = int(os.environ.get("PORT", 3000))
-    app.run(host='0.0.0.0', port=port)
-
+    app.run(host='0.0.0.0', port=port, debug=False, use_reloader=False)
